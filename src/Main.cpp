e420eada156cc2f2ef235959f1dcb66f33d4736d
Joacoeid <joaquin.eid.mier@gmail.com>
2025-04-25 10:28:48 +0200

    little cases en el main

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,12 +7,21 @@ using namespace std;
 int main() {
     // Set random seed for random.
     srand(time(0));
-    // Start a console game.
-    // Console console;
-    // console.start();
 
-    // Start a Graphics game>
-    Graphics graphics;
-    graphics.start();
+    string answer;
+    cout << "Do you wanna make the simulation on SFML (s) or on the console (c) ? (s/c)"<<endl;
+    cin >> answer;
+
+    if (answer == "s") {
+        Graphics graphics;
+        graphics.start();
+    } else if (answer == "c") {
+        Console console;
+        console.start();
+    } else {
+        cout << "Invalid answer" << endl;
+        return -1;
+    }
+
     return 0;
 }
